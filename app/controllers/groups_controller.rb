075327b62cf3dd56b1322f283cb425791fa60495
zengzheng <zengzheng2009@gmail.com>
2016-09-29 20:10:12 +0800

    add post button

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,7 +14,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    find_group_and_check_permission
   end
 
   def create
@@ -29,7 +28,6 @@ class GroupsController < ApplicationController
   end
 
   def update
-    find_group_and_check_permission
 
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
@@ -39,7 +37,6 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    find_group_and_check_permission
 
     @group.destroy
     flash[:alert] = "Group deleted"
